dc0d0c57618b45c3480f2cf465391d5d479f84ac
lenkei.akos <lenkei.akos@virgo.hu>
2018-12-09 23:08:39 +0100

    missed new files

---
+++ src/app/components/imagelist-header/imagelist-header.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, Input, ViewChild, ElementRef, EventEmitter } from '@angular/core';
+import { MatAutocomplete, MatAutocompleteSelectedEvent } from '@angular/material';
+import { FormControl } from '@angular/forms';
+import {ImageCrudService, ImageQuery} from '../../services/image-crud.service';
+
+@Component({
+  selector: 'hvr-imagelist-header',
+  templateUrl: './imagelist-header.component.html',
+  styleUrls: ['./imagelist-header.component.less']
+})
+export class ImagelistHeaderComponent implements OnInit {
+
+    @Input() avalaibleTags: string[];
+
+    public selectedTags: string[] = [];
+    public filteredTags: string[];
+    public tagFormControl  = new FormControl();
+
+    @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
+    @ViewChild('auto') matAutocomplete: MatAutocomplete;
+    constructor(public imageService: ImageCrudService) {
+        this.tagFormControl.valueChanges.subscribe(value => {
+            this.setFilteredTagList(value);
+        });
+    }
+
+    ngOnInit() {
+    }
+
+    selectTag(event: MatAutocompleteSelectedEvent) {
+        console.log('selectTag', event.option.value);
+        this.tagInput.nativeElement.value = '';
+        this.tagFormControl.setValue(null);
+        this.selectedTags.push(event.option.value);
+        const selectedTag = event.option.value;
+        const querySnapShot = this.imageService.query$.getValue();
+        querySnapShot.tags.push(selectedTag);
+        this.imageService.query$.next(querySnapShot);
+    }
+    removeTag(tagToRemove: string) {
+        console.log(tagToRemove);
+        const querySnapShot = this.imageService.query$.getValue();
+        querySnapShot.tags = querySnapShot.tags.filter(tag => tag !== tagToRemove);
+        this.imageService.query$.next(querySnapShot);
+    }
+    setFilteredTagList(value) {
+        console.log(value);
+        if (!value) {
+            this.filteredTags = this.avalaibleTags.filter(tag => this.selectedTags.indexOf(tag) === -1);
+            return;
+        }
+        this.filteredTags = this.avalaibleTags.filter(tag => {
+            return this.selectedTags.indexOf(tag) === -1 && tag.indexOf(value) !== -1;
+        });
+    }
+
+}
